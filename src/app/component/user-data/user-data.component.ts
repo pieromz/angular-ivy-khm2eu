fcdf79e8eb0be5371ab5cfc7cf35de1ffa5e9067
pieromz <pieromartuccizecca@gmail.com>
2021-12-28 12:11:04 +0100

    1 file modified, 1 added

--- src/app/component/user-data/user-data.component.ts
+++ src/app/component/user-data/user-data.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../class/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
-  styleUrls: ['./user-data.component.css']
+  styleUrls: ['./user-data.component.css'],
 })
 export class UserDataComponent implements OnInit {
+  dettaglioUtente: User;
 
-  constructor() { }
+  constructor(
+    private service: UserService,
+    private route: ActivatedRoute,
+    private routeNavigate: Router
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.dettaglioUtente = this.service.getUserById(+params.id); //+ implica il cast a number
+    });
   }
-
 }
